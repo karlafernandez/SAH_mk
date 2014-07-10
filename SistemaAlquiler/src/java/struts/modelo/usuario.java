1d7164876665b0f78ecf2be9be8a4e43a8aca392
karlafernandez <karla.m.f.f@gmail.com>
2014-07-10 00:01:15 -0500

    struts implementacion
    implementacion en el login

---
+++ SistemaAlquiler/src/java/struts/modelo/usuario.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package struts.modelo;
+
+/**
+ *
+ * @author Karla
+ */
+public class usuario {
+    private String usuario;
+    private String clave;
+    
+    public usuario() {
+        
+    }
+    
+    public String getClave() {
+        return clave;
+    }
+    
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    public String getUsuario() {
+        return usuario;
+    }
+    
+    public void setUsuario (String usuario) {
+        this.usuario = usuario;
+    }
+}
